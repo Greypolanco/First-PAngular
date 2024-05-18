c5955aa4eaf42a680f8cfa123d2a6b9f6bb0f94b
Greypolanco <greilyntejada03@gmail.com>
2024-05-18 08:53:16 +0800

    Agregando todo los modulos a los import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ClientesComponent } from './clientes/clientes.component';
-import { DireccionComponent } from './direccion/direccion.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ClientesComponent } from '../app/modules/clientes/clientes/clientes.component';
+import { DireccionComponent } from './modules/direcciones/direccion/direccion.component';
+import { ClientesModule } from './modules/clientes/clientes.module';
+import { DireccionesModule } from './modules/direcciones/direcciones.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ClientesComponent, DireccionComponent],
+  imports: [RouterOutlet,ClientesModule, DireccionesModule, RouterModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
