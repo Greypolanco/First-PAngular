0678d9cdca38867f8be1254f4390ffe651130274
Greypolanco <greilyntejada03@gmail.com>
2024-05-23 18:58:01 -0400

    Colocando ruta de detalle de direccion.

--- src/app/modules/direcciones/direcciones-routing.module.ts
+++ src/app/modules/direcciones/direcciones-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DireccionComponent } from './direccion/direccion.component';
+import { DireccionesDatailComponent } from './direcciones-datail/direcciones-datail.component';
 
 const routes: Routes = [
   {
     path: 'direcciones',
     component: DireccionComponent,
   },
-
+  {
+    path: 'direccion/:id',
+    component:DireccionesDatailComponent
+  }
 ];
 
 @NgModule({
