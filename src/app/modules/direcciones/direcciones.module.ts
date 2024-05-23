8f6914b970c59807782fe89dd95c0035ea795cec
Greypolanco <greilyntejada03@gmail.com>
2024-05-23 18:59:10 -0400

    Agregando componente de  detalle de direcciones en el modulo

--- src/app/modules/direcciones/direcciones.module.ts
+++ src/app/modules/direcciones/direcciones.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DireccionesRoutingModule } from './direcciones-routing.module';
 import { DireccionComponent } from './direccion/direccion.component';
+import { DireccionesDatailComponent } from './direcciones-datail/direcciones-datail.component';
 
 
 @NgModule({
   declarations: [
-    DireccionComponent
+    DireccionComponent,
+    DireccionesDatailComponent
   ],
   imports: [
     CommonModule,
