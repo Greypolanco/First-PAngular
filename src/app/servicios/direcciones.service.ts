b8bd0dec2aa0017bc52714f0aec862221c14b6ea
Greypolanco <greilyntejada03@gmail.com>
2024-06-06 13:54:12 -0400

    Agregando Servicios de Direcciones

--- src/app/servicios/direcciones.service.ts
+++ src/app/servicios/direcciones.service.ts
@@ -1,85 +1,54 @@
 import { Injectable } from '@angular/core';
 import { Direccion } from '../models/direccion';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DireccionesService {
-  direcciones: Direccion[] = [
-    {
-      addressId: 0,
-      alias: 'Casa',
-      street: 'Calle 1',
-      city: 'Ciudad 1',
-      state: 'Estado 1',
-      zipCode: '12345',
-      country: 'País 1',
-      active: true,
-      createAt: '2021-07-01',
-      clienteId: 0
-    },
-    {
-      addressId: 1,
-      alias: 'Trabajo',
-      street: 'Calle 2',
-      city: 'Ciudad 2',
-      state: 'Estado 2',
-      zipCode: '54321',
-      country: 'País 2',
-      active: false,
-      createAt: '2021-07-01',
-      clienteId: 2
-    },
-    {
-      addressId: 2,
-      alias: 'Otro',
-      street: 'Calle 3',
-      city: 'Ciudad 3',
-      state: 'Estado 3',
-      zipCode: '67890',
-      country: 'País 3',
-      active: true,
-      createAt: '2021-07-01',
-      clienteId: 1
-    },
-    {
-      addressId: 3,
-      alias: 'Casa',
-      street: 'Calle 4',
-      city: 'Ciudad 4',
-      state: 'Estado 4',
-      zipCode: '09876',
-      country: 'País 4',
-      active: false,
-      createAt: '2021-07-01',
-      clienteId: 3
-    },
-    {
-      addressId: 4,
-      alias: 'Trabajo',
-      street: 'Calle 5',
-      city: 'Ciudad 5',
-      state: 'Estado 5',
-      zipCode: '54321',
-      country: 'País 5',
-      active: true,
-      createAt: '2021-07-01',
-      clienteId: 4
-    }
-  ];
-  constructor() { }
+  private baseurl: string = "https://report-api.somee.com/api/addresses"
+  constructor( private http : HttpClient) { }
 
   getDireccion(): Observable<Direccion[]>{
-    return of(this.direcciones);
+    return this.http.get<Direccion[]>(this.baseurl).pipe(
+      map(response => response),
+      catchError(error => {
+        console.error(error);
+        return of([])
+      })
+    )
   }
 
   getDireccionById(addressId: number): Observable<Direccion | undefined> {
-    const direccion = this.direcciones.find(
-      (direccion) => direccion.addressId === addressId
-    );
-    return of(direccion);
+    return this.http.get<Direccion>(`${this.baseurl}/${addressId}`).pipe(
+      map(response => response),
+      catchError(error => {
+        console.error(error);
+        return of()
+      })
+    )
   }
 
-  postDireccion(){}
+  createDireccion(direccion: Direccion): Observable<Direccion> {
+    return this.http.post<Direccion>(`${this.baseurl}`, direccion).pipe(
+      map(response => response),
+      catchError(error => {
+        console.error('Error:', error);
+        return of();
+      })
+    );
+  }
+
+  editarDireccion(direccion: Partial<Direccion>): Observable<Direccion> {
+    return this.http.post<Direccion>(`${this.baseurl}`, direccion).pipe(
+      map(response => response),
+      catchError(error => {
+        console.error('Error:', error);
+        return of();
+      })
+    );
+  }
+
+  
 }
