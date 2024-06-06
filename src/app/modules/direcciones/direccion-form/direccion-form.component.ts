cd6c02e966f915d1cf186f6320c2a680ba9ef4c7
Greypolanco <greilyntejada03@gmail.com>
2024-06-06 13:57:22 -0400

    Agregando las funciones de Obtener Direcciones, Crear direcciones, Editar direcciones

--- src/app/modules/direcciones/direccion-form/direccion-form.component.ts
+++ src/app/modules/direcciones/direccion-form/direccion-form.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { error } from 'node:console';
 import { Cliente } from '../../../models/cliente';
 import { ClientesService } from '../../../servicios/clientes.service';
+import { ActivatedRoute } from '@angular/router';
+import { Direccion } from '../../../models/direccion';
+import { DireccionesService } from '../../../servicios/direcciones.service';
 
 @Component({
   selector: 'app-direccion-form',
@@ -12,10 +15,14 @@ import { ClientesService } from '../../../servicios/clientes.service';
 export class DireccionFormComponent {
   formulario : FormGroup;
   clientes: Cliente[];
+  direccion: Direccion;  
 
   constructor(
     private fb: FormBuilder,
-  private _clientesService: ClientesService){}
+    private _clientesService: ClientesService,
+    private routerManager: ActivatedRoute,
+    private _servicio: DireccionesService
+  ){}
 
   ngOnInit(){
     this.formulario = this.fb.group({
@@ -31,13 +38,39 @@ export class DireccionFormComponent {
     });
 
     this.getClientes();
+
+    this.routerManager.params.subscribe(params =>{
+      if(params['id']){
+        this._servicio.getDireccionById(+params['id']).subscribe({
+          next: (value) => {
+            this.direccion = (value);
+            this.formulario.patchValue(this.direccion);
+          },
+          error: (error) => {
+            console.log('Error al obtener la dirección', error);
+          },
+        })
+      }
+    })
   }
   
   onSubmit(){
     this.formulario.markAllAsTouched()
-
     console.info('valor del formulario',this.formulario.value)
     console.info('validez del formulario',this.formulario.valid)
+    this.formulario.markAllAsTouched();
+    if (this.formulario.valid) {
+      const direccion = this.formulario.value;
+      if (this.direccion) {
+        this._servicio.editarDireccion(direccion).subscribe(() => {
+          alert('Direccion actualizada');
+        });
+      } else {
+        this._servicio.createDireccion(direccion).subscribe(() => {
+          alert('Cliente creado');
+        });
+      }
+    }
   }
 
   getClientes(){
