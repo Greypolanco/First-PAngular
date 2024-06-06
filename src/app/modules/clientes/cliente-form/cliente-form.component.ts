50bb8bb8af3318de53901fc6e7be74f0d2a5ed94
Greypolanco <greilyntejada03@gmail.com>
2024-06-06 12:44:02 -0400

    Arreglando el CreatedAt en el formulario de cliente y Agregando el servicio ts de cliente form .

--- src/app/modules/clientes/cliente-form/cliente-form.component.ts
+++ src/app/modules/clientes/cliente-form/cliente-form.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ClientesService } from '../../../servicios/clientes.service';
+import { Cliente } from '../../../models/cliente';
 
 
 
@@ -10,8 +13,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ClienteFormComponent {
   formulario: FormGroup;
+  cliente : Cliente;
 
-  constructor(private fb: FormBuilder){}
+
+  constructor(private fb: FormBuilder, 
+    private routeManager: ActivatedRoute,
+    private _servicio: ClientesService){}
 
   ngOnInit(){
     this.formulario = this.fb.group({
@@ -19,19 +26,41 @@ export class ClienteFormComponent {
       lastName:['',Validators.required],
       cedula:['',[Validators.required,Validators.minLength(11),Validators.maxLength(11)]],
       birthDate:[new Date(),Validators.required],
-      createAt:[new Date(),Validators.required],
+      createdAt:[new Date(),Validators.required],
       email:['',[Validators.required, Validators.email]],
       active:[true,Validators.required],
     })
+    
+    this.routeManager.params.subscribe((params) => {
+      if (params['id']) {
+        this._servicio.getClienteById(+params['id']).subscribe({
+          next: (value) => {
+            this.formulario.patchValue(value);
+          },
+          error: (error) => {
+            console.log('Error al obtener el cliente', error);
+          },
+        });
+      }
+    });
   }
 
   onSubmit(){
-    this.formulario.markAllAsTouched()
-
-    //this.formulario.patchValue()
-
-    console.info('valor del formulario',this.formulario.value)
-    console.info('validez del formulario',this.formulario.valid)
+    this.formulario.markAllAsTouched();
+    console.info('Valor del formulario: ', this.formulario.value);
+    console.info('Validez del formulario: ', this.formulario.valid);
+    if (this.formulario.valid) {
+      const cliente = this.formulario.value;
+      if (this.cliente) {
+        this._servicio.editarCliente(cliente).subscribe(() => {
+          alert('Cliente actualizado');
+        });
+      } else {
+        this._servicio.createCliente(cliente).subscribe(() => {
+          alert('Cliente creado');
+        });
+      }
+    }
   }
 
   public getError(controlName: string, name: string){
