3b727a50e2e5d68fdb273e7039db5cba17d28f10
Greypolanco <greilyntejada03@gmail.com>
2024-06-06 12:45:45 -0400

    Eliminando Arreglo de cliente y haciendo nuevos metodos para cliente.

--- src/app/servicios/clientes.service.ts
+++ src/app/servicios/clientes.service.ts
@@ -1,82 +1,55 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from '../models/cliente';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { response } from 'express';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientesService {
-  clientes : Cliente[]= [
-    {
-      clienteId:0, 
-      name:"Grey", 
-      lastName:"Polanco", 
-      cedula:"000-0000000-0", 
-      birthDate: new Date("2003-01-08"),
-      createAT: new Date("2021-01-08"),
-      email:"greilyn123@gmail.com",
-      active:true,
-      addresses: []
-    },
-    {
-      clienteId:1, 
-      name:"Jasson", 
-      lastName:"Tejada", 
-      cedula:"000-0000000-0", 
-      birthDate: new Date("2003-02-20"),
-      createAT: new Date("2021-01-08"),
-      email:"levid123@gmail.com",
-      active:false,
-      addresses: []
-    },
-    {
-      clienteId:2, 
-      name:"Joseph", 
-      lastName:"Garcia", 
-      cedula:"000-0000000-0", 
-      birthDate: new Date("2003-03-24"),
-      createAT: new Date("2021-01-08"),
-      email:"Joseph123@gmail.com",
-      active:false,
-      addresses: []
-    },
-    {
-      clienteId:3, 
-      name:"Jorge", 
-      lastName:"Toribio", 
-      cedula:"000-0000000-0", 
-      birthDate: new Date("2003-04-27"),
-      createAT: new Date("2021-01-08"),
-      email:"jorge123@gmail.com",
-      active:false,
-      addresses: []
-    },
-    {
-      clienteId:4, 
-      name:"Carolina", 
-      lastName:"Brito", 
-      cedula:"000-0000000-0", 
-      birthDate: new Date("2003-01-05"),
-      createAT: new Date("2021-01-08"),
-      email:"caro123@gmail.com",
-      active:false,
-      addresses: []
-    },
-  ]
+  private baseurl: string = "https://report-api.somee.com/api/clientes"
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
-    return of(this.clientes);
+    return this.http.get<Cliente[]>(this.baseurl).pipe(
+      map(response => response),
+      catchError(error => {
+        console.error(error);
+        return of([])
+      })
+    )
   }
 
   getClienteById(clienteId: number): Observable<Cliente | undefined> {
-    console.log("hola")
-    const cliente = this.clientes.find(
-      (cliente) => cliente.clienteId === clienteId
-    );
-    return of(cliente);
+    return this.http.get<Cliente>(`${this.baseurl}/${clienteId}`).pipe(
+      map(response => response),
+      catchError(error => {
+        console.error(error);
+        return of()
+      })
+    )
   }
 
-  postCliente(){}
+  createCliente(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(`${this.baseurl}`, cliente).pipe(
+      map(response => response),
+      catchError(error => {
+        console.error('Error:', error);
+        return of();
+      })
+    );
+  }
+
+  editarCliente(cliente: Partial<Cliente>): Observable<Cliente> {
+    return this.http.post<Cliente>(`${this.baseurl}`, cliente).pipe(
+      map(response => response),
+      catchError(error => {
+        console.error('Error:', error);
+        return of();
+      })
+    );
+  }
+
 }
